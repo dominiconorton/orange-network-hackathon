9b6ec5a48bb80daf484b724e8d8770efc882df62
Dominic Norton <dnorton@dominiconorton.co.uk>
2024-11-14 06:32:50 +0000

    html at root

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Serve static files from the frontend directory
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, 'frontend')));
 
 // Serve index.html at the root URL
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/index.html'));
+    res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
 });
 
 app.use(express.json());
